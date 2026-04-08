05a36b4569d3775bbe12a3b15f247c804afbf3eb
Hadymohamed22 <Hadymohamed20051022@gmail.com>
2026-04-08 17:49:21 +0200

    feat(setup): setup project & translation & next-themes

--- src/shared/providers/index.tsx
+++ src/shared/providers/index.tsx
@@ -1,18 +1,28 @@
+"use client";
+import {
+  AbstractIntlMessages,
+  Locale,
+  NextIntlClientProvider,
+} from "next-intl";
 import { ThemeProvider } from "./components/theme-provider";
 
 type Props = {
   children: React.ReactNode;
+  messages: AbstractIntlMessages;
+  locale: Locale;
 };
 
-export default function Providers({ children }: Props) {
+export default function Providers({ children, messages, locale }: Props) {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      {children}
-    </ThemeProvider>
+    <NextIntlClientProvider locale={locale} messages={messages}>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </NextIntlClientProvider>
   );
 }
