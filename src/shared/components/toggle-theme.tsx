c06a2bf87e6d5e4be697dfeff4dfad59c8b2959e
Hadymohamed22 <Hadymohamed20051022@gmail.com>
2026-04-10 11:35:52 +0200

    refactor(dev): translate static text in toggleTheme button

--- src/shared/components/toggle-theme.tsx
+++ src/shared/components/toggle-theme.tsx
@@ -9,8 +9,13 @@ import {
   DropdownMenuTrigger,
 } from "@/shared/ui/dropdown-menu";
 import { Moon, Sun } from "lucide-react";
+import { useTranslations } from "next-intl";
 
 export default function ToggleTheme() {
+  // Translation
+  const t = useTranslations("shared.toggle-theme");
+
+  // Hooks
   const { setTheme } = useTheme();
 
   return (
@@ -20,18 +25,18 @@ export default function ToggleTheme() {
           <Button variant="toggleTheme" size="icon">
             <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
             <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-            <span className="sr-only">Toggle theme</span>
+            <span className="sr-only">{t("text")}</span>
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end">
           <DropdownMenuItem onClick={() => setTheme("light")}>
-            Light
+            {t("light")}
           </DropdownMenuItem>
           <DropdownMenuItem onClick={() => setTheme("dark")}>
-            Dark
+            {t("dark")}
           </DropdownMenuItem>
           <DropdownMenuItem onClick={() => setTheme("system")}>
-            System
+            {t("system")}
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
