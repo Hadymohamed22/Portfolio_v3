be6749849ea4f822b127eea0309e6c3983b78fb7
Hadymohamed22 <Hadymohamed20051022@gmail.com>
2026-04-11 23:28:52 +0200

    feat(contact-me): translate static data in contact-me section

--- src/app/[locale]/_components/sections/contact-me/contact-me.tsx
+++ src/app/[locale]/_components/sections/contact-me/contact-me.tsx
@@ -15,8 +15,19 @@ export default async function ContactMe() {
   const contactMeInfo = await getContactInfo();
 
   return (
-    <section className="contact-me-section mt-14 mb-18 md:mt-16 md:mb-20 px-5">
-      <div className="container mx-auto shadow-sm bg-white/70 dark:bg-white/3 border border-zinc-200 dark:border-white/5 p-18 md:p-20 lg:p-24 rounded-[3rem]">
+    <section
+      className={`
+        contact-me-section relative
+        mt-14 mb-18 md:mt-16 md:mb-20 px-5
+        before:absolute
+        before:-z-10
+        before:size-80
+        before:rounded-full
+        before:bg-linear-to-br before:from-m-primary/30 before:to-m-secondary/20
+        before:top-20 before:inset-s-20
+        `}
+    >
+      <div className="container mx-auto shadow-sm bg-white/70 dark:bg-white/3 border border-zinc-200 dark:border-white/5 p-18 md:p-20 lg:p-24 rounded-[3rem] backdrop-blur-lg">
         {/* Available In Cairo */}
         {contactMeInfo.ok && contactMeInfo.data.availableInCairo && (
           <Badge variant={"full-time"} className="mb-4">
@@ -26,16 +37,18 @@ export default async function ContactMe() {
 
         {/* Title */}
         <h2 className="font-bold text-4xl md:text-5xl w-3/4 md:w-8/12 lg:w-6/12">
-          LET{"'"}S BUILD SOMETHING
-          <p className="bg-linear-to-r from-m-primary to-m-secondary rtl:from-m-secondary rtl:to-m-primary bg-clip-text text-transparent">
-            TOGETHER
-          </p>
+          {t.rich("title", {
+            p: (chunks) => (
+              <p className="bg-linear-to-r from-m-primary to-m-secondary rtl:from-m-secondary rtl:to-m-primary bg-clip-text text-transparent rtl:inline">
+                {chunks}
+              </p>
+            ),
+          })}
         </h2>
 
         {/* Sub Title */}
-        <p className="text-base md:text-lg font-inter rtl:font-tajawal w-6/12 md:w-4/12 my-6 md:my-8 text-gray-500 dark:text-gray-400">
-          Have a project in mind? Reach out and let{"'"}s discuss how we can
-          bring your vision to life.
+        <p className="text-base md:text-lg font-inter rtl:font-tajawal w-11/12 md:w-4/12 my-6 md:my-8 text-gray-500 dark:text-gray-400">
+          {t("sub-title")}
         </p>
 
         {/* Contact Info */}
