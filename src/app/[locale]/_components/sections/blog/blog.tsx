62e7d1663c59506f4c1f910f5650bfcfad7f69c2
Hadymohamed22 <Hadymohamed20051022@gmail.com>
2026-04-18 14:30:41 +0200

    feat(blog): make blog section in progress until add auth logic to make show all blogs must user signin

--- src/app/[locale]/_components/sections/blog/blog.tsx
+++ src/app/[locale]/_components/sections/blog/blog.tsx
@@ -9,7 +9,7 @@ export default function Blog() {
   const t = useTranslations("home.blog");
 
   return (
-    <section className="blog-section my-14 md:my-16">
+    <section className="blog-section my-14 md:my-16 relative before:absolute before:z-10 before:size-full before:bg-gray-100/25 dark:before:bg-gray-100/5 before:backdrop-blur-2xl before:content-['In_Progress'] rtl:before:content-['تحت_التطوير'] before:font-bold before:-top-4 before:inset-s-0 before:flex before:items-center before:justify-center before:text-3xl md:before:text-4xl">
       <div className="container mx-auto px-5">
         {/* Section Header */}
         <SectionHeader
