396f1584f9ee106eef684f5ea36135f5ead688c3
Hadymohamed22 <Hadymohamed20051022@gmail.com>
2026-05-01 17:46:33 +0300

    feat(services-page): translate static content in hero section in services page

--- src/app/[locale]/services/_components/services-hero.tsx
+++ src/app/[locale]/services/_components/services-hero.tsx
@@ -1,27 +1,35 @@
 import SectionHeader from "@/shared/components/section-header";
 import CTA from "../../_components/sections/about/_components/cta";
+import { useTranslations } from "next-intl";
 
 export default function ServicesHero() {
+  // Translations
+  const t = useTranslations("services.hero");
+
   return (
     <section className="my-14 md:my-16">
       {/* CAPABILITIES */}
       <p className="text-cyan-500 dark:text-cyan-400 text-xs mb-4 tracking-[.3rem] uppercase font-jetbrains-mono rtl:font-tajawal font-semibold">
-        CAPABILITIES
+        {t("capabilities")}
       </p>
 
       {/* Section Header */}
       <SectionHeader
-        title={"What I Can Do For You"}
-        subTitle={
-          "Precision-engineered digital solutions at the intersection of high-end editorial design and robust technical architecture."
-        }
-        titleClasses="text-7xl md:text-8xl w-4/5 md:w-3/5 lg:w-2/5"
+        title={t.rich("title", {
+          span: (chunks) => (
+            <span className="bg-linear-to-r from-m-primary to-m-secondary rtl:from-m-secondary rtl:to-m-primary bg-clip-text text-transparent">
+              {chunks}
+            </span>
+          ),
+        })}
+        subTitle={t("description")}
+        titleClasses="text-7xl md:text-8xl w-4/5 md:w-3/5 lg:w-2/5 rtl:lg:w-3/5"
         subTitleClasses="w-3/4 md:w-2/5"
         className="mb-2!"
       />
 
       {/* CTA */}
-      <CTA talkText="Let's Start Together" />
+      <CTA talkText={t("lets-start-together")} />
     </section>
   );
 }
