cd3b52cfb319ce37c18774822e751c63c7e0434b
Hadymohamed22 <Hadymohamed20051022@gmail.com>
2026-05-03 11:52:02 +0300

    feat(contact-page): translate ContactForm component

--- src/app/[locale]/contact/_components/contact-form.tsx
+++ src/app/[locale]/contact/_components/contact-form.tsx
@@ -2,8 +2,12 @@ import { Button } from "@/shared/ui/button";
 import { Field, FieldLabel } from "@/shared/ui/field";
 import { Input } from "@/shared/ui/input";
 import { Textarea } from "@/shared/ui/textarea";
+import { useTranslations } from "next-intl";
 
 export default function ContactForm() {
+  // Translations
+  const t = useTranslations("contact.contact-form");
+
   return (
     <form className="p-10 pb-14 md:p-12 md:pb-16 shadow-lg bg-white dark:bg-white/3 border border-transparent dark:border-white/10 rounded-xl flex flex-col gap-8">
       {/* Name And Email */}
@@ -11,15 +15,19 @@ export default function ContactForm() {
         {/* Name Input */}
         <Field>
           <FieldLabel htmlFor="Name" className="capitalize">
-            Full Name
+            {t("full-name")}
           </FieldLabel>
-          <Input type="text" placeholder="Hady Mohamed" id="Name" />
+          <Input
+            type="text"
+            placeholder={t("full-name-placeholder")}
+            id="Name"
+          />
         </Field>
 
         {/* Email Input */}
         <Field>
           <FieldLabel htmlFor="Email" className="capitalize">
-            EMAIL ADDRESS
+            {t("email-address")}
           </FieldLabel>
           <Input type="email" placeholder="hadysapry60@gmail.com" id="Email" />
         </Field>
@@ -28,17 +36,19 @@ export default function ContactForm() {
       {/* Textarea Input */}
       <Field>
         <FieldLabel htmlFor="Message" className="capitalize">
-          MESSAGE
+          {t("message")}
         </FieldLabel>
         <Textarea
-          placeholder="Tell me about your project vision..."
+          placeholder={t("message-placeholder")}
           id="Message"
           className="min-h-44"
         />
       </Field>
 
       {/* Send Button */}
-      <Button type="submit">Send Now</Button>
+      <Button type="submit" className="w-fit px-10! md:px-14! cursor-pointer">
+        {t("send-now")}
+      </Button>
     </form>
   );
 }
