ad96c23657617284139f05e2be015c43749d9dd2
Hadymohamed22 <Hadymohamed20051022@gmail.com>
2026-05-04 17:28:01 +0300

    feat(projects-page): translate projects page hero title

--- src/app/[locale]/projects/page.tsx
+++ src/app/[locale]/projects/page.tsx
@@ -2,14 +2,24 @@ import SectionHeader from "@/shared/components/section-header";
 import FiltersTabs from "../_components/sections/projects/_components/filters-tabs";
 import ProjectsFiltersTabsSkeleton from "./_skeletons/projects-filters-tabs.skeleton";
 import ProjectDetailBox from "./_components/project-detail-box";
+import { useTranslations } from "next-intl";
 
 export default function Page() {
+  // Translations
+  const t = useTranslations("projects");
+
   return (
     <main className="projects-page">
       <div className="container mx-auto px-5">
         {/* Header */}
         <SectionHeader
-          title={"Selected Work"}
+          title={t.rich("title", {
+            span: (chunks) => (
+              <span className="bg-linear-to-r from-m-primary to-m-secondary rtl:from-m-secondary rtl:to-m-primary bg-clip-text text-transparent">
+                {chunks}
+              </span>
+            ),
+          })}
           titleClasses="text-7xl md:text-8xl w-full md:w-5/6 lg:w-4/6 rtl:lg:w-3/5"
           className="mb-6! mt-12"
         />
